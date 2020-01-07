7c90f1dc1e263724acfde64ec5110d80a1383b2f
zRagnnar <57605654+zRagnnar@users.noreply.github.com>
2020-01-07 20:55:28 -0300

    Adicionar contas

--- very/d86ouchebag.js
+++ very/d86ouchebag.js
@@ -3,16 +3,28 @@ var words = new BuildArray(NumberOfWords)
 
 // Use the following variables to 
 // define your random words:
-words[1] = "g.lesadeaxa@gmail.com:Gerald1968" 
-words[2] = "ced752@yahoo.com:Cindy1969" 
-words[3] = "cshawnburns@gmail.com:critter1" 
-words[4] = "deannaimmel@gmail.com:10swords" 
-words[5] = "hhgs83@hotmail.com:Hector83" 
-words[6] = "g.lesadeaxa@gmail.com:Gerald1968" 
-words[7] = "lily_mikova@yahoo.com:loveacs1_1" 
-words[8] = "g.lesadeaxa@gmail.com:Gerald1968" 
-words[9] = "felipsyfera@yahoo.com.br:flp55555" 
-words[10] = "g.lesadeaxa@gmail.com:Gerald1968" 
+words[1] = "https://enrt.eu/C1tLg"
+words[2] = "https://enrt.eu/C1tLg"
+words[3] = "https://enrt.eu/PYuLpE5"
+words[4] = "https://enrt.eu/rfCEpnG4"
+words[5] = "https://enrt.eu/yOOhp"
+words[6] = "https://enrt.eu/BWmKh7"
+words[8] = "https://enrt.eu/qjpr"
+words[9] = "https://enrt.eu/AR6LanHR"
+words[10] = "https://enrt.eu/GTBsT7"
+words[11] = "https://enrt.eu/ZSF1O3ZO"
+words[12] = "https://enrt.eu/rBHK"
+words[13] = "https://enrt.eu/id146wy"
+words[14] = "https://enrt.eu/RTnWR"
+words[15] = "https://enrt.eu/bGEO"
+words[16] = "https://enrt.eu/EPcTnxj"
+words[17] = "https://enrt.eu/9O8X"
+words[18] = "https://enrt.eu/NTQ3X"
+words[19] = "https://enrt.eu/w76k"
+words[20] = "https://enrt.eu/9FvL7g"
+words[21] = "https://enrt.eu/WTuDieQo"
+words[22] = "https://enrt.eu/rzYd"
+words[23] = "https://enrt.eu/yDst"
 
 function BuildArray(size){
 this.length = size
