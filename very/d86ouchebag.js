383f363a3515ca47694a0cbdb2a6ea2f4cf3b1d8
zRagnnar <57605654+zRagnnar@users.noreply.github.com>
2020-01-09 18:16:47 -0300

    Adicionar Contas

--- very/d86ouchebag.js
+++ very/d86ouchebag.js
@@ -25,10 +25,42 @@ words[20] = "https://enrt.eu/9FvL7g"
 words[21] = "https://enrt.eu/WTuDieQo"
 words[22] = "https://enrt.eu/rzYd"
 words[23] = "https://enrt.eu/yDst"
+words[24] = "https://enrt.eu/HwK99"
+words[25] = "https://enrt.eu/xRiYFbK"
+words[26] = "https://enrt.eu/kogU"
+words[27] = "https://enrt.eu/1EeJOQ"
+words[28] = "https://enrt.eu/RCCJIf"
+words[29] = "https://enrt.eu/YViFAXUo"
+words[30] = "https://enrt.eu/AtEBA"
+words[31] = "https://enrt.eu/M5FXU"
+words[32] = "https://enrt.eu/1Bhw"
+words[33] = "https://enrt.eu/MWqvN"
+words[34] = "https://enrt.eu/WlS2RV9"
+words[35] = "https://enrt.eu/qTRMTjvM"
+words[36] = "https://enrt.eu/Ndql"
+words[37] = "https://enrt.eu/Yjat"
+words[38] = "https://enrt.eu/3HxUu5W"
+words[39] = "https://enrt.eu/l2R362Ag"
+words[40] = "https://enrt.eu/l2R362Ag"
+words[41] = "https://enrt.eu/Xdgc7I"
+words[42] = "https://enrt.eu/46ke"
+words[43] = "https://enrt.eu/fr4fZ"
+words[44] = "https://enrt.eu/gH6I"
+words[45] = "https://enrt.eu/dNsmyWTW"
+words[46] = "https://enrt.eu/7O6M8"
+words[47] = "https://enrt.eu/4m0N"
+words[48] = "https://enrt.eu/7OczN8T"
+words[49] = "https://enrt.eu/nnav6"
+words[50] = "https://enrt.eu/cF74W"
+words[51] = "https://enrt.eu/cF74W"
+words[52] = "https://enrt.eu/nvMJ43g"
+words[53] = "https://enrt.eu/SnIY2Fw"
+words[54] = "https://enrt.eu/vZOdf"
 
 function BuildArray(size){
 this.length = size
 for (var i = 1; i <= size; i++){
+this[i] = null}
 this[i] = null}
 return this
 }
